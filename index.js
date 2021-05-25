a29ba66686d3119f2cc79a9adcd69274815d5d06
99lalo <74028591+99lalo@users.noreply.github.com>
2021-05-25 15:01:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req,res) => {
+    res.send("Up and running with graphql crash course")
+})
+
+app.listen(8082, () => console.log("Running at 8082"))
